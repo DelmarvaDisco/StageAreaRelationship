06826b729707e361ca6106bde254aa57005863e1
FloodHydrology <cnjones7@ua.edu>
2022-06-21 14:44:09 -0500

    data prep

--- R/0_data_Prep.R
+++ R/0_data_Prep.R
@@ -1,8 +1,8 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Title: DEM Prep
+#Title: Data Prep
 #Coder: C. Nathan Jones (cnjones7@ua.edu)
 #Date: 6/21/2022
-#Purpose: Prep DEM for analysis at Jones Road
+#Purpose: Prep DEM and site for analysis at Jones Road
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -12,6 +12,7 @@
 rm(list=ls(all=TRUE))
 
 #Download packages of interest 
+library(readxl)
 library(stars)
 library(fasterize)
 library(whitebox)
@@ -25,6 +26,7 @@ p<-"+proj=utm +zone=17 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
 #Download relevant data 
 dem<-raster("data/I_raw/2007_1m_DEM.tif")
 jr_clip<-st_read("data/I_raw/JR_boundary.shp")
+jr_sites<-read_xlsx('data/I_raw/Site_directory_Core.xlsx', sheet = "Baltimore Corner Catchment")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #2.0 Clip DEM to workspace -----------------------------------------------------
@@ -40,5 +42,26 @@ jr_proj<-st_transform(jr_clip, p)
 jr_buffer<-st_buffer(jr_proj, dist = 500)
 
 #Crop DEM
-dem<-crop(dem, jr_buffer)
+dem_crop<-crop(dem, jr_buffer)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#3.0 Convert site data to sf object --------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+jr_sites<- jr_sites %>%
+  filter(str_detect(`Site Name`,"Wetland")) %>% 
+  mutate(
+    Longitude = as.numeric(Longitude),
+    Latitude = as.numeric(Latitude)
+  ) %>% 
+  st_as_sf( 
+    coords = c("Longitude", "Latitude"), 
+    crs = '+proj=longlat +datum=WGS84 +no_defs') %>% 
+  st_transform(., p)
+
+mapview(dem_crop) + mapview(jr_sites)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#4.0 Export data---------------------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+writeRaster(dem_c, 'data/III_output/dem_jr.tif')
+st_write(jr_sites, 'data/III_output/jr_sites.shp')
